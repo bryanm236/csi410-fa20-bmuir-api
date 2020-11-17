65ac0d1c9b594f882c953e9d2eb297de05e307c6
bryanm236 <bmuir@rams.colostate.edu>
2020-11-16 23:15:48 -0700

    11/16 /signup

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+
 const db = require('./dbConnectExec.js')
 
 const app = express();
+app.use(express.json())
 
 
 app.get("/hi",(req,res)=>{
@@ -14,6 +17,44 @@ app.get("/hi",(req,res)=>{
 
 })
 
+app.post("/CustomerT", async (req,res)=>{
+    // res.send("creating user")
+    console.log("request body", req.body)
+
+    var FirstName = req.body.FirstName;
+    var LastName = req.body.LastName;
+    var Email = req.body.Email;
+    var Password = req.body.Password;
+
+    if(!FirstName || !LastName || !Email || !Password){
+        return res.status(400).send("bad request")
+    }
+
+    FirstName = FirstName.replace("'","''")
+    LastName = LastName.replace("'","''")
+
+    var emailCheckQuery = `SELECT Email
+    FROM CustomerT
+    WHERE Email = '${Email}'`
+
+    var existingUser = await db.executeQuery(emailCheckQuery)
+
+    // console.log("existing user", existingUser)
+    if(existingUser[0]) {
+        return res.status(409).send('Please enter a different email.')
+    }
+
+    var hashedPassword = bcrypt.hashSync(Password)
+
+    var insertQuery = `INSERT INTO CustomerT(FirstName,LastName,Email,Password)
+    VALUES('${FirstName}','${LastName}','${Email}','${hashedPassword}')`
+
+    db.executeQuery(insertQuery).then(()=>{res.status(201).send()})
+    .catch((err)=>{
+        console.log("error in POST /CustomerT",err)
+        res.status(500).send()
+    })
+})
 
 // app.post()
 // app.put()
