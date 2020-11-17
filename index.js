13bcc78e463047b569c69344b300dae72152088f
bryanm236 <bmuir@rams.colostate.edu>
2020-11-16 21:12:05 -0700

    11/16/20

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/hi",(req,res)=>{
 // app.put()
 // app.delete()
 
-app.get("/DriverT", (req,res)=>{
+app.get("/VehicleT", (req,res)=>{
     //get data from database
     db.executeQuery(`SELECT *
     FROM VehicleT
@@ -39,4 +39,27 @@ app.get("/DriverT", (req,res)=>{
 
 })
 
+app.get("/VehicleT/:pk", (req, res)=> {
+    var pk = req.params.pk
+    // console.log("my PK:" , pk)
+
+    var myQuery = `SELECT *
+    FROM VehicleT
+    LEFT JOIN DriverT
+    ON VehicleT.VehiclePK = DriverT.VehicleFK
+    WHERE VehiclePK = ${pk}`
+
+    db.executeQuery(myQuery)
+        .then((drivers)=> {
+            // console.log("Drivers: ", drivers)
+
+            if(drivers[0]){
+                res.send(drivers[0])
+            }else{res.status(404).send('bad request')}
+        })
+        .catch((err)=>{
+            console.log("Error in /VehicleT/pk", err)
+            res.status(500).send()
+        })
+})
 app.listen(5000,()=>{console.log("app is running on port 5000")})
